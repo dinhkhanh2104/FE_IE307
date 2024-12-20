56ea98b2600a8bb58ea4400d1272e94f66d7587a
dinhkhanh <trankhanh210403@gmail.com>
2024-12-20 11:17:37 +0700

    fix conflict

--- src/navigators/HomeNavigator.js
+++ src/navigators/HomeNavigator.js
@@ -2,10 +2,11 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../screens/Home";
 import ProductDetail from "../screens/ProductDetail"
 import CartNavigator from "./CartNavigator";
-import Checkout from "../screens/CheckOut";
+import Checkout from "../screens/Checkout";
 import AddressSelection from "../screens/AddressSelection ";
 import AddAddressScreen from "../screens/AddAddress";
 import EditAddressScreen from "../screens/EditAddress";
+import SaleNavigator from "./SaleNavigator";
 
 const Stack = createStackNavigator()
 
@@ -39,6 +40,34 @@ const HomeNavigator = () => {
                 component={SaleNavigator}
                 options={{
                     headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="CheckOut"
+                component={Checkout}
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="AddressSelection"
+                component={AddressSelection}
+                options={{
+                headerShown:false
+                }}
+            />
+            <Stack.Screen
+                name="AddAddress"
+                component={AddAddressScreen}
+                options={{
+                headerShown:false
+                }}
+            />
+            <Stack.Screen
+                name="EditAddress"
+                component={EditAddressScreen}
+                options={{
+                headerShown:false
                 }}
             />
         </Stack.Navigator>
